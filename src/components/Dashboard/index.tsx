83f623cc90f8004a0d78debf0b9b4b71ed8a6c6d
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-05-11 14:46:57 -0300

    correcting component name

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { Summary } from "../Summary";
-import { TransectionsTable } from "../TransactionsTable";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
-      <TransectionsTable />
+      <TransactionsTable />
     </Container>
   );
 }
